b33ea8a6a4be26a12edbe88eb1459f9e89df61e6
Showkat Ali <71689825+Showkiip@users.noreply.github.com>
2024-07-19 16:26:12 +0500

    update the service provider

--- src/ImageUploadServiceProvider.php
+++ src/ImageUploadServiceProvider.php
@@ -9,8 +9,8 @@ class ImageUploadServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/image-upload.php' => config_path('image-upload.php'),
-        ]);
+            __DIR__ . '/../config/image-upload.php' => config_path('image-upload.php'),
+        ], 'config');
     }
 
     public function register()
