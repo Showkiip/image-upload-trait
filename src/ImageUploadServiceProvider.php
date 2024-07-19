141c2b264786ab2dab8dd34e2f1c36b5998edc23
Showkat Ali <71689825+Showkiip@users.noreply.github.com>
2024-07-19 17:30:57 +0500

    add config

--- src/ImageUploadServiceProvider.php
+++ src/ImageUploadServiceProvider.php
@@ -6,27 +6,18 @@ use Illuminate\Support\ServiceProvider;
 
 class ImageUploadServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        // Merge package configuration with application's copy
-        $this->mergeConfigFrom(__DIR__ . '/../config/image-upload.php', 'image-upload');
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        // Publish configuration file
         $this->publishes([
-            __DIR__ . '/../config/image-upload.php' => config_path('image-upload.php'),
+            __DIR__.'/../config/image-upload.php' => config_path('image-upload.php'),
         ], 'config');
+
+        // Merge package configuration with application's copy
+        $this->mergeConfigFrom(__DIR__.'/../config/image-upload.php', 'image-upload');
+    }
+
+    public function register()
+    {
+        // Register bindings, if any
     }
 }
